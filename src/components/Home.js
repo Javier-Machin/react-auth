baaae9dbf9c22980a4ab60c0b2bb75a737b94418
Javier Machin <javier_machin_acosta@hotmail.com>
2019-02-04 17:29:54 +0000

    Implement login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
+    const { isAuthenticated, login } = this.props.auth;
     return (
       <div>
-        <h1>HOME</h1>
+        <h1>Home</h1>
+        { isAuthenticated ? ( 
+            <Link to="/profile">View profile</Link>
+        ) : (
+          <button onClick={login}>Log In</button>
+        )}
       </div>
-    )
+    );
   }
 }
 
