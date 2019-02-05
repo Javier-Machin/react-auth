5c2f2272b908bcb20ccf7c1de4016c7096464905
Javier Machin <javier_machin_acosta@hotmail.com>
2019-02-05 10:01:24 +0000

    Add user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 class Profile extends React.Component {
+  state = {
+    profile: null,
+    error: ""
+  };
+
+  componentDidMount() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.props.auth.getProfile( 
+      (profile, error) => this.setState({ profile, error})
+    );
+  }
+
   render() {
+    const {profile} = this.state;
+    if (!profile) return null;
+
     return (
-      <div>
-        <h1>PROFILE</h1>
-      </div>
-    )
+      <>
+        <h1>Profile</h1>
+        <p>{profile.nickname}</p>
+        <img 
+          style={{ maxWidth: 50, maxHeight: 50}}
+          src={profile.picture}
+          alt="profile pic"
+        />
+        <pre>{JSON.stringify(profile, null, 2)}</pre>
+      </>
+    );
   }
 }
 
